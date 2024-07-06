c5c8b51fbe1a06d29f50ccae9ca98bb65030eb9f
Anish <anish.singh@quadbtech.com>
2024-07-06 21:49:02 +0530

    adding img uploading functioanlity

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import cors from "cors";
 import dotenv from 'dotenv'
-
 import { connectToMongoDB } from './config.js';
 import router from './routes/AuthRoutes.js';
 import ProductRouter from './routes/ProductRoutes.js';
-import CartRoute from './routes/CartRoute.js';
-import OrderRoute from './routes/OrderRoute.js';
 
 
 dotenv.config()
@@ -30,5 +27,3 @@ app.listen(process.env.PORT, () => {
 
 app.use('/auth', router);
 app.use('/product', ProductRouter);
-app.use('/cart', CartRoute);
-app.use('/order', OrderRoute);
--- routes/OrderRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { placeOrderController } from "../controller/PlaceOrderController.js";
-
-const OrderRoute = express.Router()
-
-OrderRoute.post('/placeOrder', placeOrderController)
-
-export default OrderRoute;
